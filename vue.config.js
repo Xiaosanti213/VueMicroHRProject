1b083118e723609869d7156ffe86dd883da0a5a7
Xiaosanti213 <xiaosanti213@gmail.com>
2021-05-26 23:45:59 +0800

    First Commit

---
+++ vue.config.js
@@ -0,0 +1,38 @@
+let proxyObj = {};
+const CompressionPlugin = require("compression-webpack-plugin");
+proxyObj['/ws'] = {
+    ws: true,
+    target: "ws://localhost:8081"
+};
+proxyObj['/'] = {
+    ws: false,
+    target: 'http://localhost:8081',
+    changeOrigin: true,
+    pathRewrite: {
+        '^/': ''
+    }
+}
+module.exports = {
+    devServer: {
+        host: 'localhost',
+        port: 8080,
+        proxy: proxyObj
+    },
+    publicPath: './',
+    outputDir: "dist", // 输出文件目录
+    lintOnSave: false, // eslint 是否在保存时检查
+    assetsDir: 'static', // 配置js、css静态资源二级目录的位置  
+    configureWebpack: config => {
+        if (process.env.NODE_ENV === 'production') {
+            return {
+                plugins: [
+                    new CompressionPlugin({
+                        test: /\.js$|\.html$|\.css/,
+                        threshold: 1024,
+                        deleteOriginalAssets: false
+                    })
+                ]
+            }
+        }
+    }
+}
